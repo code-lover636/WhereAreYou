67a497257d8b7538f3cddb9c1c41bf6e08610173
code-lover636 <aravindashokan636@gmail.com>
2024-06-24 19:00:37 +0530

    requirements.txt

--- website.py
+++ website.py
@@ -4,12 +4,11 @@ import json, streamlit as st
 st.title("Who Are You?")
 
 col1, col2 = st.columns(2)
+# data = main()
+with open("data.json", "r") as file:
+    data = json.load(file)
 
 with col1:
-    # data = main()
-    with open("data.json", "r") as file:
-        data = json.load(file)
-
     st.html(f"""
     <b>IP Address:</b> {data["ip_address"]} <br>
     <b>City:</b>  {data["city"]}<br>
@@ -20,5 +19,10 @@ with col1:
     <b>Flag:</b>  {data["flag"]["emoji"]}<br>
     <b>Time Zone: {data["timezone"]["name"]}<br>
     <b>Current Time:</b>  {data["timezone"]["current_time"]}<br>
-    <img src="{data["flag"]["png"]}" alt="flag">
+    
     """)
+# 
+with col2:
+    st.html(f'<img src="{data["flag"]["png"]}" alt="flag" height=230>')
+
+st.map(latitude=str(data["latitude"]), longitude=str(data["longitude"]))
